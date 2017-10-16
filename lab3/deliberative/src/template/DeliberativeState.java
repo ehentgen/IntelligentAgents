a6493052a407adb1e7746bbdbe53c2ab4c39bc7a
Emily Hentgen <emily.hentgen@epfl.ch>
2017-10-16 22:06:47 +0200

    continue state and BFS implementation

--- lab3/deliberative/src/template/DeliberativeState.java
+++ lab3/deliberative/src/template/DeliberativeState.java
@@ -1,83 +1,155 @@
 package template;
 
-import logist.simulation.Vehicle;
-
-import java.awt.geom.RectangularShape;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import logist.agent.Agent;
-import logist.behavior.DeliberativeBehavior;
 import logist.plan.Action;
+import logist.plan.Action.Delivery;
 import logist.plan.Action.Move;
-import logist.plan.Plan;
+import logist.plan.Action.Pickup;
 import logist.task.Task;
-import logist.task.TaskDistribution;
-import logist.task.TaskSet;
-import logist.topology.Topology;
 import logist.topology.Topology.City;
 
 /**
- * Our algo (in order to get a plan) will travel through all those possibles plans
+ * Our algo (in order to get a plan) will travel through all those possibles
+ * plans
+ * 
  * @author Jean-Thomas
  *
  */
 public class DeliberativeState {
-	
-	private TaskSet remainingTasks;
-	private TaskSet executedTasks;
-	private List<Action> actionsPerformed;
-	
-	private City currentCity;
-	
-	private int currentCharge = 0;
-	
-	public DeliberativeState(TaskSet remaining, TaskSet executed, City current, List<Action> wayToHere) {
-		this.remainingTasks = remaining;
-		this.executedTasks = executed;
-		this.currentCity = current;
-		this.actionsPerformed = wayToHere; //This will be super-useful in order to find how to reach the best state
-	}
-	
-	
-	public List<DeliberativeState> getSuccessors() {
-		List<DeliberativeState> nextPossibleStates = null;
-		
-		for (Task task : remainingTasks) {
-			
-			//remove the task we will execute
-			TaskSet newRemaining = TaskSet.copyOf(remainingTasks);
-			newRemaining.remove(task);
-			
-			//and add it to the executed list
-			TaskSet newExecuted = TaskSet.copyOf(executedTasks);
-			newRemaining.add(task);
-			
-			City newCity = task.deliveryCity;
 
-			List<City> cities = task.path();			
-			for (City city : cities) {
-				Move a = new Move(city);
-				actionsPerformed.add(a);
-			}
-			
-			DeliberativeState s = new DeliberativeState(newRemaining, newExecuted, newCity, actionsPerformed);
-			nextPossibleStates.add(s); //avertissement ?
+    private Set<Task> notPickedUpTasks;
+    private Set<Task> pickedUpTasks;
+    private Set<Task> deliveredTasks;
+
+    private List<Action> actionsPerformed;
+
+    private City departure;
+
+    private double cost;
+    private double charge;
+
+    public DeliberativeState(Set<Task> notPickedUpTasks,
+	    Set<Task> pickedUpTasks, Set<Task> deliveredTasks, City departure,
+	    double charge, double cost, List<Action> wayToHere) {
+
+	this.notPickedUpTasks = notPickedUpTasks;
+
+	this.pickedUpTasks = pickedUpTasks;
+	this.deliveredTasks = deliveredTasks;
+
+	this.departure = departure;
+	this.cost = cost;
+	this.charge = charge;
+
+	// This will be super-useful in order to find how to reach the best
+	// state
+	if (wayToHere == null) {
+	    this.actionsPerformed = new ArrayList<Action>();
+	} else {
+	    this.actionsPerformed = wayToHere;
+	}
+    }
+
+    public List<DeliberativeState> getSuccessors(Agent agent) {
+	List<DeliberativeState> nextPossibleStates = new ArrayList<DeliberativeState>();
+
+	double costPerKm = agent.vehicles().get(0).costPerKm();
+	double currentCost = cost;
+
+	for (Task task : notPickedUpTasks) {
+	    double updatedCharge = charge + task.weight;
+
+	    // in the next state, try picking up task, if possible
+	    if (updatedCharge < agent.vehicles().get(0).capacity()) {
+		// remove the task we will pick up
+		Set<Task> newNotPickedUpTasks = new HashSet<Task>(
+			notPickedUpTasks);
+		newNotPickedUpTasks.remove(task);
+
+		// and add it to the picked up tasks list
+		Set<Task> newPickedUpTasks = new HashSet<Task>(deliveredTasks);
+		newPickedUpTasks.add(task);
+
+		double updatedCost = currentCost
+			+ departure.distanceTo(task.pickupCity) * costPerKm;
+
+		City destination = task.pickupCity;
+
+		DeliberativeState s = new DeliberativeState(
+			newNotPickedUpTasks, newPickedUpTasks, deliveredTasks,
+			destination, updatedCharge, updatedCost,
+			actionsPerformed);
+		nextPossibleStates.add(s);
+
+		List<City> path = departure.pathTo(destination);
+		for (City city : path) {
+		    actionsPerformed.add(new Move(city));
 		}
-		return nextPossibleStates;
+		actionsPerformed.add(new Pickup(task));
+	    }
+
 	}
-	
-	
-	/**
-	 * This method could be useful to compare final states and choose the best one
-	 * @return
-	 */
-	public double getGain() {
-		return executedTasks.rewardSum();
+
+	for (Task task : pickedUpTasks) {
+	    // remove the picked up but not yet delivered task
+	    Set<Task> newPickedUpTasks = new HashSet<Task>(pickedUpTasks);
+	    newPickedUpTasks.remove(task);
+
+	    // and add it to the delivered tasks set
+	    Set<Task> newDeliveredTasks = new HashSet<Task>(deliveredTasks);
+	    newDeliveredTasks.add(task);
+
+	    double updatedCharge = charge - task.weight;
+	    double updatedCost = currentCost
+		    + departure.distanceTo(task.deliveryCity) * costPerKm;
+
+	    City destination = task.deliveryCity;
+
+	    DeliberativeState s = new DeliberativeState(notPickedUpTasks,
+		    newPickedUpTasks, newDeliveredTasks, destination,
+		    updatedCharge, updatedCost, actionsPerformed);
+	    nextPossibleStates.add(s);
+
+	    List<City> path = departure.pathTo(destination);
+	    for (City city : path) {
+		actionsPerformed.add(new Move(city));
+	    }
+	    actionsPerformed.add(new Delivery(task));
 	}
-	
-	// => There is only one possible final state.
-	public boolean isFinalState() {
-		return (remainingTasks.isEmpty());
+
+	return nextPossibleStates;
+    }
+
+    public double cost() {
+	return cost;
+    }
+
+    /**
+     * This method could be useful to compare final states and choose the best
+     * one
+     * 
+     * @return
+     */
+    public double getGain() {
+	double rewardSum = 0;
+	for (Task task : deliveredTasks) {
+	    rewardSum += task.reward;
 	}
+	return rewardSum;
+    }
+
+    // => There is only one possible final state.
+    public boolean isFinalState() {
+	return (notPickedUpTasks.isEmpty() && pickedUpTasks.isEmpty());
+    }
+
+    public List<Action> actionsPerformed() {
+	return actionsPerformed;
+    }
 
 }
--- lab3/deliberative/src/template/DeliberativeTemplate.java
+++ lab3/deliberative/src/template/DeliberativeTemplate.java
@@ -1,21 +1,21 @@
 package template;
 
 /* import table */
-import logist.simulation.Vehicle;
-
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import cern.colt.Arrays;
 import logist.agent.Agent;
 import logist.behavior.DeliberativeBehavior;
+import logist.plan.Action;
 import logist.plan.Plan;
+import logist.simulation.Vehicle;
 import logist.task.Task;
 import logist.task.TaskDistribution;
 import logist.task.TaskSet;
 import logist.topology.Topology;
 import logist.topology.Topology.City;
-
 
 /**
  * An optimal planner for one vehicle.
@@ -23,143 +23,175 @@ import logist.topology.Topology.City;
 @SuppressWarnings("unused")
 public class DeliberativeTemplate implements DeliberativeBehavior {
 
-	enum Algorithm {BFS, ASTAR, NAIVE}
-	
-	/* Environment */
-	Topology topology;
-	TaskDistribution td;
-	
-	/* the properties of the agent */
-	Agent agent;
-	int capacity;
+    enum Algorithm {
+	BFS, ASTAR, NAIVE
+    }
 
-	/* the planning class */
-	Algorithm algorithm;
-	
-	@Override
-	public void setup(Topology topology, TaskDistribution td, Agent agent) {
-		this.topology = topology;
-		this.td = td;
-		this.agent = agent;
-		
-		// initialize the planner
-		int capacity = agent.vehicles().get(0).capacity();
-		String algorithmName = agent.readProperty("algorithm", String.class, "NAIVE");
-		
-		// Throws IllegalArgumentException if algorithm is unknown
-		algorithm = Algorithm.valueOf(algorithmName.toUpperCase());
-		
-		// ...
-	}
-	
-	@Override
-	public Plan plan(Vehicle vehicle, TaskSet tasks) {
-		Plan plan;
+    /* Environment */
+    Topology topology;
+    TaskDistribution td;
 
-		// Compute the plan with the selected algorithm.
-		switch (algorithm) {
-		case ASTAR:
-			// ...
-			System.out.println("Running ASTAR Algo");
-			plan = naivePlan(vehicle, tasks);
-			break;
-		case BFS:
-			// ...
-			System.out.println("Running BFS Algo");
-			plan = bfsPlan(vehicle, tasks);
-			break;
-		case NAIVE:
-			plan = naivePlan(vehicle, tasks);
-			break;			
-		default:
-			throw new AssertionError("Should not happen.");
-		}		
-		return plan;
+    /* the properties of the agent */
+    Agent agent;
+    int capacity;
+
+    /* the planning class */
+    Algorithm algorithm;
+
+    @Override
+    public void setup(Topology topology, TaskDistribution td, Agent agent) {
+	this.topology = topology;
+	this.td = td;
+	this.agent = agent;
+
+	// initialize the planner
+	int capacity = agent.vehicles().get(0).capacity();
+	String algorithmName = agent.readProperty("algorithm", String.class,
+		"NAIVE");
+
+	// Throws IllegalArgumentException if algorithm is unknown
+	algorithm = Algorithm.valueOf(algorithmName.toUpperCase());
+
+	// ...
+    }
+
+    @Override
+    public Plan plan(Vehicle vehicle, TaskSet tasks) {
+	Plan plan;
+
+	// Compute the plan with the selected algorithm.
+	switch (algorithm) {
+	case ASTAR:
+	    // ...
+	    System.out.println("Running ASTAR Algo");
+	    plan = naivePlan(vehicle, tasks);
+	    break;
+	case BFS:
+	    // ...
+	    System.out.println("Running BFS Algo");
+	    plan = bfsPlan(vehicle, tasks);
+	    break;
+	case NAIVE:
+	    plan = naivePlan(vehicle, tasks);
+	    break;
+	default:
+	    throw new AssertionError("Should not happen.");
 	}
-	
-	/**
-	 * -> Q represents the states we still have to go through <br>
-	 * -> C represents the states we already go through, and don't want to cycle through (again) <br>
-	 * -> n is just a state <br>
-	 * 
-	 * More than a state-based BFS, this function should keep track of the best path, in order to compute the plan afterwards.<br>
-	 * the best way of doing it is probably to keep this in the state itself as a List\Action\ (how did I come here?)
-	 * 
-	 * @param vehicle
-	 * @param tasks
-	 * @return
-	 */
-	private Plan bfsPlan(Vehicle vehicle, TaskSet tasks) {
-		List<DeliberativeState> Q = null;
-		Q.add(new DeliberativeState(tasks, null, vehicle.getCurrentCity(), null)); // initial node
-		
-		List<DeliberativeState> loopCheck = null;
-		List<DeliberativeState> S = null;
-		DeliberativeState n;
-		
-		
-		while (!Q.isEmpty()) {
-			/* n <- first elem of Q && Q <- rest(Q) */
-			n = Q.remove(0);
-			
-			if (n.isFinalState())
-				System.out.println("Return n. To implement"); //TODO
-			
-			if (!loopCheck.contains(n)) { //?? only null?
-				loopCheck.add(n);
-				
-				/* S <- successors(n) */
-				S = (n.getSuccessors());
-			}
-			
-			Q.addAll(S);
-			
-			/* if Q is empty, return Failure */
-			if (Q.isEmpty())
-				System.out.println("Q is empty ! Error in BFS!");
-				System.exit(0);
+	return plan;
+    }
+
+    /**
+     * -> Q represents the states we still have to go through <br>
+     * -> C represents the states we already went through, and don't want to
+     * cycle through (again) <br>
+     * -> n is just a state <br>
+     * 
+     * More than a state-based BFS, this function should keep track of the best
+     * path, in order to compute the plan afterwards.<br>
+     * the best way of doing it is probably to keep this in the state itself as
+     * a List\Action\ (how did I come here?)
+     * 
+     * @param vehicle
+     * @param tasks
+     * @return
+     */
+    private Plan bfsPlan(Vehicle vehicle, TaskSet tasks) {
+	// add the tasks the vehicle might be carrying if the initial plan has
+	// been cancelled and a new one is being recomputed
+	tasks.addAll(agent.vehicles().get(0).getCurrentTasks());
+
+	// initialize the starting state of the BFS algorithm
+	List<DeliberativeState> Q = new ArrayList<DeliberativeState>();
+	Set<Task> notPickedUpTasks = tasks;
+	Set<Task> pickUpTasks = new HashSet<Task>();
+	Set<Task> deliveredTasks = new HashSet<Task>();
+	DeliberativeState initialState = new DeliberativeState(
+		notPickedUpTasks, pickUpTasks, deliveredTasks,
+		vehicle.getCurrentCity(), 0, 0, null); // initial node
+	Q.add(initialState);
+
+	List<DeliberativeState> loopCheck = new ArrayList<DeliberativeState>();
+	List<DeliberativeState> S = null;
+	DeliberativeState currentState;
+
+	// stores the final cost in the agent's plan resulting from the BFS
+	// algorithm (which is also the minimum cost)
+	Double minimumCost = Double.MAX_VALUE;
+	// stores the final state in the agent's plan resulting from the BFS
+	// algorithm
+	DeliberativeState finalState = null;
+
+	while (!Q.isEmpty()) {
+	    /* n <- first elem of Q && Q <- rest(Q) */
+	    currentState = Q.remove(0);
+
+	    // a plan where all tasks are delivered has been found
+	    if (currentState.isFinalState()) {
+		// check whether this is the best plan so far
+		if (currentState.cost() < minimumCost) {
+		    minimumCost = currentState.cost();
+		    finalState = currentState;
 		}
-		
-		
-		return buildPlan();
-	}
-	
-	private Plan buildPlan() {
-		Plan p = new Plan(null);
-		//TODO
-		return p;
-	}
+	    }
 
-	private Plan naivePlan(Vehicle vehicle, TaskSet tasks) {
-		City current = vehicle.getCurrentCity();
-		Plan plan = new Plan(current);
+	    if (!loopCheck.contains(currentState)) { // ?? only null?
+		loopCheck.add(currentState);
+		/* S <- successors(n) */
+		S = currentState.getSuccessors(agent);
+	    }
 
-		for (Task task : tasks) {
-			// move: current city => pickup location
-			for (City city : current.pathTo(task.pickupCity))
-				plan.appendMove(city);
+	    Q.addAll(S);
 
-			plan.appendPickup(task);
-
-			// move: pickup location => delivery location
-			for (City city : task.path())
-				plan.appendMove(city);
-
-			plan.appendDelivery(task);
-
-			// set current city
-			current = task.deliveryCity;
-		}
-		return plan;
+	    // blah?
+	    /* if Q is empty, return Failure */
+	    // if (Q.isEmpty())
+	    // System.out.println("Q is empty ! Error in BFS!");
+	    // System.exit(0);
 	}
 
-	@Override
-	public void planCancelled(TaskSet carriedTasks) {
-		
-		if (!carriedTasks.isEmpty()) {
-			// This cannot happen for this simple agent, but typically
-			// you will need to consider the carriedTasks when the next
-			// plan is computed.
-		}
+	return buildPlan(finalState);
+    }
+
+    private Plan buildPlan(DeliberativeState state) {
+	City initialCity = agent.vehicles().get(0).getCurrentCity();
+	Plan plan = new Plan(initialCity);
+	List<Action> actions = state.actionsPerformed();
+	for (Action action : actions) {
+	    plan.append(action);
 	}
+	return plan;
+    }
+
+    private Plan naivePlan(Vehicle vehicle, TaskSet tasks) {
+	City current = vehicle.getCurrentCity();
+	Plan plan = new Plan(current);
+
+	for (Task task : tasks) {
+	    // move: current city => pickup location
+	    for (City city : current.pathTo(task.pickupCity))
+		plan.appendMove(city);
+
+	    plan.appendPickup(task);
+
+	    // move: pickup location => delivery location
+	    for (City city : task.path())
+		plan.appendMove(city);
+
+	    plan.appendDelivery(task);
+
+	    // set current city
+	    current = task.deliveryCity;
+	}
+	return plan;
+    }
+
+    @Override
+    public void planCancelled(TaskSet carriedTasks) {
+
+	if (!carriedTasks.isEmpty()) {
+	    // This cannot happen for this simple agent, but typically
+	    // you will need to consider the carriedTasks when the next
+	    // plan is computed.
+	}
+    }
 }
